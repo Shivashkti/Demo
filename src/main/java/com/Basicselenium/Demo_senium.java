5b922b59f9302e1447cdcf4291d0b71dea842dc7
Shivashkti <yadavshivashkti@gmail.com>
2024-07-23 17:15:15 +0530

    This is new project

---
+++ src/main/java/com/Basicselenium/Demo_senium.java
@@ -0,0 +1,19 @@
+package com.Basicselenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Demo_senium {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.selenium.dev/downloads/");
+		Thread.sleep(3000);
+		driver.close();
+
+	}
+}
